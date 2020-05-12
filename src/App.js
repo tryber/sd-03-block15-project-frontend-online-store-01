d97aeb3726afc351267074422371ab66b4348de3
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-12 15:38:14 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import CampoBusca from './components/CampoBusca';
 
 function App() {
   return (
-    <div className="App">
-      <CampoBusca />
-    </div>
+    <div className="App" />
   );
 }
 
