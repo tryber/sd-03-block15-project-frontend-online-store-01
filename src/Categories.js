934c8f2f5ee6959470cf287d2e19c3ee05f31f26
Clayton Pereira <cpereiramt@gmail.com>
2020-05-12 19:06:53 -0400

    salvando alteraçoes no projeto

--- src/Categories.js
+++ src/Categories.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Api from './services/api';
+import {getCategories} from './services/api';
 
 class Categories extends React.Component {
-
   render() {
+    const categorias = getCategories().then((array) => array.map(element => console.log(element)));
+    console.log( categorias);
     return (
       <div>
         <ul data-testid="category">
           <h4>Categorias</h4>
+         {/*  {categorias.map((element) => console.log(element))} */}
           <li>
             categorias
           </li>
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,11 @@
 export async function getCategories() {
   // implement here
+  let categorias = [];
+   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json())
+    .then((data) => data);
 }
+
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
