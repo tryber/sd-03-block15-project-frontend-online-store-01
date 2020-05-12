4338e8a5c379ee721191b1a0b6c7d45052ad4891
Clayton Pereira <cpereiramt@gmail.com>
2020-05-12 19:59:50 -0400

    Salvando alteraçoes na branch

--- src/Categories.js
+++ src/Categories.js
@@ -2,14 +2,27 @@ import React from 'react';
 import {getCategories} from './services/api';
 
 class Categories extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categorias: [],
+        }
+    }
+   componentDidMount() {
+    this.categoriasLoad();
+   }
+
+    categoriasLoad() {
+       getCategories().then((array) => array.map(element => 
+        element.id)).then(dados => this.setState({categorias: dados}));
+   }
+
   render() {
-    const categorias = getCategories();
-    console.log(categorias.then((array) => array.map(element => console.log(element))));
+       console.log(this.state.categorias);
     return (
       <div>
         <ul data-testid="category">
           <h4>Categorias</h4>
-         {/*  {categorias.map((element) => console.log(element))} */}
           <li>
             categorias
           </li>
