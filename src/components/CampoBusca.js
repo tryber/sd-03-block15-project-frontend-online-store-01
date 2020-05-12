c305e3dc6903ce586206f0ce3571ce7344da2688
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-12 15:33:04 -0300

    adicionando input, mas sem requisiçao

---
+++ src/components/CampoBusca.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class CampoBusca extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { item: '' };
+    this.textChange = this.textChange.bind(this);
+  }
+
+  textChange(value) {
+    this.setState({ item: value.target.value });
+  }
+
+  textInput() {
+    const { item } = this.state;
+    if (item === '') {
+      return (
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      );
+    }
+    return <p>{item}</p>;
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.textChange} />
+        {this.textInput()}
+      </div>
+    );
+  }
+}
