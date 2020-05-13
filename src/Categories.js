4acc571f4c581947fc491d3c7c01298414bf5751
Clayton Pereira <cpereiramt@gmail.com>
2020-05-13 07:55:46 -0400

    Resolvendo problemas no requisito 4

--- src/Categories.js
+++ src/Categories.js
@@ -4,29 +4,25 @@ import { getCategories } from './services/api';
 class Categories extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      categorias: [],
-    }
+    this.state = { categorias: [] };
   }
+
   componentDidMount() {
     this.categoriasLoad();
   }
 
   categoriasLoad() {
-    getCategories().then((array) => array.map(element =>
-      element)).then(dados => this.setState({ categorias: dados }));
+    getCategories().then((array) =>
+      array.map((element) => element)).then((dados) => this.setState({ categorias: dados }));
   }
 
   render() {
-   const {categorias} = this.state; 
+    const { categorias } = this.state;
     return (
       <div>
-        <ul >
+        <ul>
           <h4>Categorias</h4>
-
-          {categorias.map(element =>
-            <li data-testid="category" key={element.id}>{element.name}</li>)}
-
+          {categorias.map((element) => <li data-testid="category" key={element.id}>{element.name}</li>)}
         </ul>
       </div>
     );
