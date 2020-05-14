ca7e00c625d6607d47f168b133e553a9622aea5e
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-14 15:04:49 -0300

    trocando de branch

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
+const initialState = {
+  menssagem: 'Mensagem (opcional)',
+  ratingValue: null,
+};
 
 export default class Rating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menssagem: 'Mensagem (opcional)',
+      ratingValue: null,
+    };
+    this.submit = this.submit.bind(this);
+  }
+
+  setRating(value) {
+    this.setState({ ratingValue: value });
+  }
+
+  submit(event) {
+    event.preventDefault();
+    getProductsFromCategoryAndQuery();
+    const comments = localStorage.getItem('comments');
+    localStorage.setItem('comments', [comments, this.state.ratingValue]);
+    this.setState({ initialState });
+  }
+
   render() {
     const { menssagem } = this.state;
+    const comments = localStorage.getItem('comments');
     return (
       <div>
-        <form>
+        <form onSubmit={this.submit}>
           <input id="email" type="email" noValidate />
           {[...Array(5)].map((star, i) => {
             const ratingValue = i + 1;
@@ -25,6 +53,7 @@ export default class Rating extends Component {
           <textarea data-testid="product-detail-evaluation" value={menssagem} cols={40} rows={10} />
           <button type="submit">Avaliar</button>
         </form>
+        {comments.split(',').join(' ')}
       </div>
     );
   }
