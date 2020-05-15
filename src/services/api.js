30cecb70ebd871a0fcc89e3be13bd41da60c72d6
Clayton Pereira <cpereiramt@gmail.com>
2020-05-14 22:40:47 -0400

    Salvando alteraçoes do metodo faltando achar uma forma de renderizar os props no componente

--- src/services/api.js
+++ src/services/api.js
@@ -10,5 +10,5 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
     .then((response) => response.json())
-    .then((data) => data);  
+    .then((data) => data);
 }
