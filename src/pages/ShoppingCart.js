e68f303fce12dea0915462055cb9e0944d02a137
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-16 19:01:19 -0300

    requisito8 passando no total 10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,6 +2,27 @@ import React from 'react';
 import caixaVazia from '../images/caixa-vazia.png';
 
 class ShoppingCart extends React.Component {
+  cardItems() {
+    const { location: { state: { cart } } } = this.props;
+    return (
+      <div>
+        {cart.map((itemsInTheCart) => (
+          <div>
+            <h1 data-testid="shopping-cart-product-name">
+              {itemsInTheCart.title}
+            </h1>
+            <h3>
+              {itemsInTheCart.price}
+            </h3>
+            <h4 data-testid="shopping-cart-product-quantity">
+              {itemsInTheCart.amountInTheCart}
+            </h4>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   render() {
     const { location: { state: { cart } } } = this.props;
     return (
@@ -9,31 +30,15 @@ class ShoppingCart extends React.Component {
         {
           cart.valueOf().length <= 0
           && (
-          <div>
-            <img src={caixaVazia} alt="caixa-vazia" />
-            <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          </div>
+            <div>
+              <img src={caixaVazia} alt="caixa-vazia" />
+              <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+            </div>
           )
         }
         {
           !cart.valueOf().length <= 0
-          && (
-            <div>
-              {cart.map((itemsInTheCart) => (
-                <div>
-                  <h1 data-testid="shopping-cart-product-name">
-                    {itemsInTheCart.title}
-                  </h1>
-                  <h3>
-                    {itemsInTheCart.price}
-                  </h3>
-                  <h4 data-testid="shopping-cart-product-quantity">
-                    {itemsInTheCart.amountInTheCart}
-                  </h4>
-                </div>
-              ))}
-            </div>
-          )
+          && this.cardItems()
         }
       </div>
     );
